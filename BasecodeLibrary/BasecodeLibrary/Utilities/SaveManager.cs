a397eae729ac26c48cdd4b50f139d2b78150690f
Anthony Russell <ynot_russell@hotmail.com>
2018-11-20 10:15:32 -0500

    Adding new json object save logic to the save manager

--- BasecodeLibrary/BasecodeLibrary/Utilities/SaveManager.cs
+++ BasecodeLibrary/BasecodeLibrary/Utilities/SaveManager.cs
@@ -1,4 +1,5 @@
 ﻿using BasecodeLibrary.Interfaces;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -81,6 +82,43 @@ namespace BasecodeLibrary.Utilities
                 Debug.WriteLine(ex.Message);
             }
             return returnValue;
+        }
+
+        /// <summary>
+        /// Checks to see if a file exists in the local application working directory 
+        /// </summary>
+        /// <param name="filename">The file name (and extension if it has one) of the file you wish to check if exists.</param>
+        /// <returns>Returns true if the file exists</returns>
+        public static async Task<bool> FileExists(string filename)
+        {
+            return await ApplicationData.Current.LocalFolder.TryGetItemAsync(filename) != null;
+        }
+
+        /// <summary>
+        /// Serializes an object to json and saves it to disk with the filename passed in. 
+        /// </summary>
+        /// <param name="fileName">The file name (and extension if you want) of the file you wish to save</param>
+        /// <param name="serializeableObject">The object you wish to serialze and save to disk</param>
+        public static async void SaveJsonFile(string fileName, object serializeableObject)
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile sampleFile = await storageFolder.CreateFileAsync(fileName, CreationCollisionOption.ReplaceExisting);
+            string settingsJson = JsonConvert.SerializeObject(serializeableObject);
+            await FileIO.WriteTextAsync(sampleFile, settingsJson);
+        }
+
+        /// <summary>
+        /// Gets a file from disk with the filename passed in of type T.
+        /// </summary>
+        /// <typeparam name="T">The type of the file you wish returned</typeparam>
+        /// <param name="filename">The name of the file saved to disk</param>
+        /// <returns>Returns an instance of type T</returns>
+        public static async Task<T> GetJsonFile<T>(string filename)
+        {
+            StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
+            StorageFile sampleFile = await storageFolder.GetFileAsync(filename);
+            string file = await FileIO.ReadTextAsync(sampleFile);
+            return JsonConvert.DeserializeObject<T>(file);
         }
     }
 
